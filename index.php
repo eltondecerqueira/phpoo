97b76d5e813c520b7fbcaf01b945ceb8395b5b7e
Elton <eltondecerqueira@gmail.com>
2019-10-04 17:31:51 -0300

    file default teste

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
-<?php
-
-
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        if (phpversion() >= 5.4):
+            echo phpversion() . ' Ola mundo, podemos programar';
+        else:
+            echo phpversion() . ' Olá mundo, termos que atualizar o php';
+                   
+        endif;
+        
+//        echo "<pre>";
+//        print_r(ini_get_all());
+//        echo "</pre>";
+        
+        echo ini_get('date.timezone') . "<br>";
+        echo date('d/m/y H:i:s');
+        //date_default_timezone_set("UTC");
+        echo date_default_timezone_get().'<br>';
+        echo date('d/m/y H:i:s');
+        
+        ?>
+    </body>
+</html>
